8b5a3932b16f4c71ddbce172d88f24ea1b717ac6
Copyes <fan_545697167@qq.com>
2017-04-22 17:42:14 +0800

    restart

--- server/model/movies.js
+++
@@ -1,6 +0,0 @@
-const db = require('../config/db.js');
-const moviesModel = require('../schema/movies.js');
-// todolist 数据库
-const TodoListDb = db.TodoList;
-// movies 这个表
-const Movies = TodoListDb.import(moviesModel);
